09c72dcb521b0e4f274bdda297da1ce368060464
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2025-12-02 20:10:54 -0300

    add s3 config

--- main.tf
+++ main.tf
@@ -10,3 +10,24 @@ terraform {
 provider "aws" {
   region = "us-east-2"
 }
+
+resource "aws_s3_bucket" "terraform-state" {
+  bucket        = "rocketseat-iac-98"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags = {
+    IAC = "True"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform-state" {
+  bucket = "rocketseat-iac-98"
+  
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
